e6a9ad1dd67c9c8c13cba4c25826577d98e40863
joanavrsilva <joanavrsilva@gmail.com>
2022-12-16 15:59:21 +0000

    Reservations models: create customer class

--- reservations/models.py
+++ reservations/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+class Customer(models.Model):
+    first_name = models.CharField(max_length=25)
+    last_name = models.CharField(max_length=25)
+    phone = models.CharField(max_length=25)
+    email = models.EmailField()
+    featured_image = CloudinaryField('image', default='placeholder')
