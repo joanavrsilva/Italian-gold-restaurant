dddd1037ec7b04fa41aeccaef80c901010b98e18
joanavrsilva <joanavrsilva@gmail.com>
2022-12-16 16:05:12 +0000

    Reservations models: add status

--- reservations/models.py
+++ reservations/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
+STATUS = ((0, "Submited"), (1, "Accepted"))
+
 class Customer(models.Model):
     first_name = models.CharField(max_length=25)
     last_name = models.CharField(max_length=25)
