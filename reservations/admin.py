26cd5afe31240e731b6b8f80c61aa92408ba8ff5
joanavrsilva <joanavrsilva@gmail.com>
2022-12-16 16:44:18 +0000

    Setup summernote

--- reservations/admin.py
+++ reservations/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import Booking
+from django_summernote.admin import SummernoteModelAdmin
 
-admin.site.register(Booking)
+@admin.register(Booking)
+class BookingAdmin(SummernoteModelAdmin):
+
+    summernote_fields = ('content')
