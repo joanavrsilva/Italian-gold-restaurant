35165890ade7a7961aec45aefd367bd7cf1bcbce
joanavrsilva <joanavrsilva@gmail.com>
2022-12-16 21:31:30 +0000

    Reservation views: create the view code

--- reservations/views.py
+++ reservations/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Booking
 
-# Create your views here.
+# Generic list view
+class BookingList(generic.ListView):
+    model = Booking
+    queryset = Booking.objects.filter(status=1).order_by('-created_on')
+    template_name = 'index.html'
+    paginate_by = 8
