3f33cf0c2462ad1fc2990f19dfb14d8a9145c2dd
joanavrsilva <joanavrsilva@gmail.com>
2022-12-19 01:49:43 +0000

    Italian restaurant urls: include name reservations

--- italian_restaurant/urls.py
+++ italian_restaurant/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("reservations.urls"), name="blog-urls"),
+    path("", include("reservations.urls"), name="reservations-urls"),
     path('summernote/', include('django_summernote.urls'))
 ]
