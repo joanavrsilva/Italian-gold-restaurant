743163a481149bbecc22b6087957846070073c05
joanavrsilva <joanavrsilva@gmail.com>
2022-12-19 05:30:50 +0000

    Reservations views: add customer form

--- reservations/views.py
+++ reservations/views.py
@@ -21,6 +21,32 @@ class BookingDetail(View):
             "booking_detail.html",
             {
                 "booking": booking,
+                "customer": customer,
+            },
+        )
+    def booking(self, request, slug, *args, **kwargs):
+
+        queryset = Booking.objects.filter(status=1)
+        booking = get_object_or_404(queryset, slug=slug)
+        customers = booking.customers.filter(approved=True).order_by("-created_on")
+
+        customer_form = CustomerForm(data=request.POST)
+        if customer_form.is_valid():
+            customer_form.instance.email = request.user.email
+            customer_form.instance.name = request.user.username
+            customer = customer_form.save(commit=False)
+            customer_form.booking = booking
+            booking.save()
+        else:
+            customer_form = CustomerForm()
+
+        return render(
+            request,
+            "booking_detail.html",
+            {
+                "booking": booking,
                 "customers": customers,
+                "commented": True,
+                "customer_form": customer_form,
             },
         )
