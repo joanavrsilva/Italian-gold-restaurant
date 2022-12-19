0997a3d50949d1fd578d5921d7ca2405bfaf2783
joanavrsilva <joanavrsilva@gmail.com>
2022-12-19 05:27:56 +0000

    Italian restaurant settings: add crispy template and message tags

--- italian_restaurant/settings.py
+++ italian_restaurant/settings.py
@@ -42,16 +42,20 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'cloudinary_storage',
-    'django.contrib.staticfiles',
-    'cloudinary',
-    'django_summernote',
-    'reservations',
     'django.contrib.sites',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'cloudinary_storage',
+    'django.contrib.staticfiles',
+    'cloudinary',
+    'django_summernote',
+    'crispy_forms',
+    'reservations',
+    
 ]
+
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -93,6 +97,16 @@ SITE_ID = 1
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+MESSAGE_TAGS = {
+        messages.DEBUG: 'alert-info',
+        messages.INFO: 'alert-info',
+        messages.SUCCESS: 'alert-success',
+        messages.WARNING: 'alert-warning',
+        messages.ERROR: 'alert-danger',
+    }
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
