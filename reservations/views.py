27af783ddf90763d10c17cbf02c4fdfba8dc41b2
joanavrsilva <joanavrsilva@gmail.com>
2022-12-19 01:56:07 +0000

    Reservations views: add booking detail view

--- reservations/views.py
+++ reservations/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views import generic
+from django.views import generic, View
 from .models import Booking
 
 # Generic list view
@@ -8,3 +8,19 @@ class BookingList(generic.ListView):
     queryset = Booking.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
     paginate_by = 6
+
+class BookingDetail(View):
+
+    def get(self, request, *args, **kwargs):
+        queryset = Booking.objects.filter(status=1)
+        booking = get_object_or_404(queryset)
+        customers = booking.customers.filter(approved=True).order_by("-created_on")
+
+        return render(
+            request,
+            "booking_detail.html",
+            {
+                "booking": booking,
+                "customers": customers,
+            },
+        )
